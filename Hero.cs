8b92ba97335424f75062f350ba4c956e063f2a5e
slawkovvl <slawkovvl@gmail.com>
2023-02-15 12:26:23 +0100

    hero class

---
+++ Hero.cs
@@ -0,0 +1,21 @@
+﻿namespace CodeWarsKatas2022
+{
+    public class Hero
+    {
+        public string Name;
+        public string Position = "00";
+        public float Health = 100;
+        public float Damage = 5;
+        public int Experience = 0;
+
+
+        public Hero(string name = "Hero")
+        {
+            Name = name;
+        }
+
+        public static int Move(int position, int roll) => position + 2 * roll;
+    }
+
+
+}
